c90e7d07aa543e4bed007c030f7851e0565f24cd
sleaz0id <mat.staszczyk@gmail.com>
2016-10-17 21:26:16 +0200

    Run command implemented

--- micro_blogger.rb
+++ micro_blogger.rb
@@ -18,6 +18,20 @@ class MicroBlogger
 
   def run
     puts "Welcome to the JSL Twitter Client!"
+    
+    command = ""
+    while command != "q"
+      printf "enter command: "
+      parts = gets.chomp.split(" ")
+      command = parts[0]
+      args = parts[1..-1].join(" ")
+      case command
+        when 't' then tweet(args) 
+        when 'q' then puts "Goodbye!"
+        else
+          puts "Sorry, I don't know how to #{command}"
+      end
+    end
   end
 
 end
